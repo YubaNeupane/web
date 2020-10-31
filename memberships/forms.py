a6f090d6a052b433172121ae0208a5c83aadc27c
James Geddes <j@jamesgeddes.pro>
2020-10-31 15:27:54 +0000

    getting closer to some useful login and settings forms

--- memberships/forms.py
+++ memberships/forms.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 from django import forms
 from .models import Member
+from django.forms import ModelForm
+from memberships.models import Member
 
 
 class DateInput(forms.DateInput):
@@ -42,8 +44,27 @@ class RegistrationForm(forms.Form):
         return birth_date
 
 
-
-
+class MemberSettingsForm(ModelForm):
+        class Meta:
+            model = Member
+            fields = ['full_name',
+                      'preferred_name',
+                      'birth_date',
+                      'profile_image',
+                      'telephone',
+                      'minecraft_username',
+                      'address_1',
+                      'gift_aid',
+                      'gdpr_likeness',
+                      'gdpr_sms_updates',
+                      'gdpr_sms_notifications',
+                      'gdpr_email_updates',
+                      'gdpr_email_notifications',
+                      'gdpr_telephone_updates',
+                      'gdpr_telephone_notifications',
+                      'gdpr_post_updates',
+                      'gdpr_post_notifications'
+                      ]
 
 
 
